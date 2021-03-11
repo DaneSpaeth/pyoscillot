dd9c1853ec0a9e037d556f0042e37d868f810c9b
Dane Marvin Spaeth <vc409@uni-heidelberg.de>
2021-03-11 14:59:21 +0100

    Refactor projecion, add first line of sight projection

--- pypulse/spherical_geometry.py
+++ pypulse/spherical_geometry.py
@@ -1,56 +1,73 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.special import sph_harm
+from mpl_toolkits.mplot3d import Axes3D
 
 
-def get_circular_phi_theta_x_z():
+def get_circular_phi_theta_x_y_z(inclination=90):
     """ Sample x and z for a circular star."""
-    phi = np.linspace(0, np.pi, 250)
-    theta = np.linspace(0, np.pi, 250)
+    N = 100
+    phi = np.linspace(0, np.pi, N)
+    factor = 2
+    theta = np.linspace(0, factor * np.pi, factor * N)
     phi, theta = np.meshgrid(phi, theta)
 
     # The Cartesian coordinates of the unit sphere
+    # phi_incl = np.mod(phi + np.radians(inclination - 90), np.pi)
+    # print(phi_incl)
     x = np.sin(phi) * np.cos(theta)
+    y = np.sin(phi) * np.sin(theta)
     z = np.cos(phi)
 
-    return phi, theta, x, z
+    return phi, theta, x, y, z
 
 
 def create_starmask(N=100):
     """ Return a starmask."""
-    _, _, x, z = get_circular_phi_theta_x_z()
+    _, _, x, y, z = get_circular_phi_theta_x_y_z()
 
-    # Remember that both x and z span from -1 to 1
-    # So factor two comes in and also the -1 factor in x_diff and z_diff
-    dN = 2 / N
-    grid = np.zeros((N, N))
-    for row in range(N):
-        for col in range(N):
-            x_grid = dN * row - 1
-            z_grid = dN * col - 1
-            x_diff = np.abs(x - x_grid)
-            z_diff = np.abs(z - z_grid)
-            x_mask = np.where(x_diff < dN, 1, 0)
-            z_mask = np.where(z_diff < dN, 1, 0)
-            mask = np.logical_and(x_mask, z_mask)
-            x_value = x[mask]
-            z_value = z[mask]
-            if not np.sum(mask):
-                star = 0
-            else:
-                star = 1
-            grid[col, row] = star
+    star = np.ones(x.shape)
+    star_mask = project_2d(x, y, z, star)
+    star_mask[np.isnan(star_mask)] = 0
+    return star_mask
+
+
+def pulsation_rad(l=1, m=1, N=100, project=True, inclination=90):
+    """ Get radial component of pulsation.
+
+
+        :param int l: Number of surface lines of nodes
+        :param int m: Number of polar lines of nodes (-l<=m<=l)
+        :param int N: Number of cells to sample
+        :param bool project: If True: Project the component onto the line of
+                                      sight
+    """
+    phi, theta, x, y, z = get_circular_phi_theta_x_y_z(inclination)
+    # Calculate the spherical harmonic Y(l,m)
+    harmonic = sph_harm(m, l, theta, phi)
+
+    if project:
+        # Add in line of sight
+        harmonic = harmonic * np.sin(theta)
+
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax.scatter(x, y, z)
+    # # ax.aspect_ratio("equal")
+    # plt.show()
+    # exit()
+
+    # TODO maybe normalize?
+    grid = project_2d(x, y, z, harmonic, N)
 
     return grid
 
 
-def get_spherical_harmonics(l=1, m=1, N=100):
-    phi, theta, x, z = get_circular_phi_theta_x_z()
-    # Calculate the spherical harmonic Y(l,m)
-    harmonic = sph_harm(m, l, theta, phi).real
+def project_2d(x, y, z, values, N=100):
+    """ Project the 2d array value on the unitsphere.
 
-    # TODO maybe normalize?
-
+        At the moment no inclination
+    """
     # Remember that both x and z span from -1 to 1
     # So factor two comes in and also the -1 factor in x_diff and z_diff
     dN = 2 / N
@@ -66,17 +83,23 @@ def get_spherical_harmonics(l=1, m=1, N=100):
             mask = np.logical_and(x_mask, z_mask)
             x_value = x[mask]
             z_value = z[mask]
-            sph_value = harmonic[mask]
-            if not len(sph_value):
-                sph_value = np.nan
+            value = values[mask]
+            if not len(value):
+                value = np.nan
             else:
-                sph_value = np.mean(sph_value)
-            grid[col, row] = sph_value
+                value = np.mean(value)
+            grid[col, row] = value
 
     return grid
 
 
 if __name__ == "__main__":
-    harm = get_spherical_harmonics()
-    plt.imshow(harm)
+    rad = pulsation_rad(l=3, m=3, N=100, project=False)
+    rad_incl = create_starmask()
+    fig, ax = plt.subplots(1, 2)
+
+    ax[0].imshow(rad.real, cmap="seismic", )
+    # rad = rad * np.exp(1j * 2 * np.pi * nu * t)
+    ax[1].imshow(rad_incl.real, cmap="seismic", )
+
     plt.show()
