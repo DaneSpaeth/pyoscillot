0d74b424c87da91e025213f727f1db1f0650ec87
Dane Marvin Spaeth <vc409@uni-heidelberg.de>
2021-03-17 12:38:27 +0100

    Add convolution (not in sim yet)

--- pypulse/utils.py
+++ pypulse/utils.py
@@ -116,6 +116,59 @@ def interpolate_to_restframe(wavelength, spectrum, rest_wavelength):
     return shift_spec
 
 
+def smear(wave, spec, R, w_sample=1):
+    '''
+    Smears a model spectrum with a gaussian kernel to the given resolution, R.
+
+    Modified from https://github.com/spacetelescope/pysynphot/issues/78
+    Parameters
+    -----------
+
+    sp: spectrum
+
+
+    R: int
+        The resolution (dL/L) to smear to
+
+    w_sample: int
+        Oversampling factor for smoothing
+
+    Returns
+    -----------
+
+    sp: PySynphot Source Spectrum
+        The smeared spectrum
+    '''
+
+    # Save original wavelength grid and units
+    w_grid = wave
+
+    # Generate logarithmic wavelength grid for smoothing
+    w_logmin = np.log10(np.nanmin(w_grid))
+    w_logmax = np.log10(np.nanmax(w_grid))
+    n_w = np.size(w_grid) * w_sample
+    w_log = np.logspace(w_logmin, w_logmax, num=n_w)
+
+    # Find stddev of Gaussian kernel for smoothing
+    R_grid = (w_log[1:-1] + w_log[0:-2]) / (w_log[1:-1] - w_log[0:-2]) / 2
+    sigma = np.median(R_grid) / R
+    if sigma < 1:
+        sigma = 1
+
+    # Interpolate on logarithmic grid
+    f_log = np.interp(w_log, w_grid, spec)
+
+    # Smooth convolving with Gaussian kernel
+    gauss = Gaussian1DKernel(stddev=sigma)
+    f_conv = convolve_fft(f_log, gauss)
+
+    # Interpolate back on original wavelength grid
+    f_sm = np.interp(w_grid, w_log, f_conv)
+
+    # Write smoothed spectrum back into Spectrum object
+    return f_sm
+
+
 if __name__ == "__main__":
 
     import dataloader as load
