fe97487cde9f869d987feec310f5c3d3a8244828
Dane Späth <dane.spaeth@gmail.com>
2021-06-23 12:11:52 +0200

    Add reading in of multiple simulation tickets

--- pypulse/parse_ini.py
+++ pypulse/parse_ini.py
@@ -25,10 +25,14 @@ def parse_ticket(ticketpath):
 
         Return parameters as dictionary.
     """
+    # Read in the ini file
     config = configparser.ConfigParser()
     config.read(ticketpath)
+    superkeys = list(config.keys())
+
+    # Read in the GLOBAL part as global parameter in first level of dictionary
     conf_dict = {}
-    conf = config["DEFAULT"]
+    conf = config["GLOBAL"]
     for key in conf:
         if "path" in key:
             conf_dict[key] = Path(conf[key])
@@ -38,9 +42,22 @@ def parse_ticket(ticketpath):
             except ValueError:
                 conf_dict[key] = conf[key]
 
+    # Save the keys of all simulations as keys
+    superkeys.pop(superkeys.index("GLOBAL"))
+    conf_dict["simulations"] = superkeys
+
+    for sim in superkeys:
+        conf = config[sim]
+        conf_dict[sim] = {}
+        for key in conf:
+            try:
+                conf_dict[sim][key] = float(conf[key])
+            except ValueError:
+                conf_dict[sim][key] = conf[key]
+
     return conf_dict
 
 
 if __name__ == "__main__":
-    conf_dict = parse_global_ini()
+    conf_dict = parse_ticket("example_ticket.ini")
     print(conf_dict)
