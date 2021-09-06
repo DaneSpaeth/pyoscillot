2116c2d2e4db94da715b79d83a839c4e98b9d027
Dane Späth <dane.spaeth@gmail.com>
2021-09-06 11:10:38 +0200

    Make main script switch automatically depending on host

--- pypulse/main.py
+++ pypulse/main.py
@@ -3,6 +3,8 @@ from simulation_controller import SimulationController
 from parse_ini import parse_global_ini, parse_ticket
 from datasaver import DataSaver
 from check_time_series import check_time_series
+import socket
+laptop = socket.gethostname() == "dane-ThinkPad-E460"
 
 
 def main(ticket):
@@ -12,19 +14,20 @@ def main(ticket):
     global_dict = parse_global_ini()
     conf_dict = parse_ticket(ticket)
 
-    # Run the Simulation
-    SimulationController(ticket)
-    # Run Serval
-    exit()
-    subprocess.run(["bash", "run_serval.sh", str(global_dict["rvlibpath"]),
-                    str(conf_dict["name"]), f"HIP{int(conf_dict['hip'])}"])
+    if not laptop:
+        # Run the Simulation
+        SimulationController(ticket)
+    else:
+        # Run Serval
+        subprocess.run(["bash", "run_serval.sh", str(global_dict["rvlibpath"]),
+                        str(conf_dict["name"]), f"HIP{int(conf_dict['hip'])}"])
 
-    # Copy the flux and the ticket to the new folders
-    saver = DataSaver(str(conf_dict["name"]))
-    saver.copy_ticket(ticket)
-    saver.copy_flux()
+        # Copy the flux and the ticket to the new folders
+        saver = DataSaver(str(conf_dict["name"]))
+        saver.copy_ticket(ticket)
+        saver.copy_flux()
 
-    check_time_series(str(conf_dict["name"]))
+        check_time_series(str(conf_dict["name"]))
 
 
 if __name__ == "__main__":
