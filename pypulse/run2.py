09bb1487b476b9335edb9f77a938050a9394fa9e
Dane Spaeth <dspaeth@srv5.lsw.uni-heidelberg.de>
2021-11-28 18:43:28 +0100

    Removethe run files

--- pypulse/run2.py
+++
@@ -1,4 +0,0 @@
-from animation_pulsation_new import create_high_res_arrays
-
-ticket = "/data/dspaeth/simulations/fake_spectra/TALK_180/talk_ticket.ini"
-create_high_res_arrays(ticket)
--- pypulse/run4.py
+++
@@ -1,4 +0,0 @@
-from animation_pulsation_new import create_high_res_arrays
-
-ticket = "/data/dspaeth/simulations/fake_spectra/TALK_0_m-1/talk_ticket.ini"
-create_high_res_arrays(ticket)
