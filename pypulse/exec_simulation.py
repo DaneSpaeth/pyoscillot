9e9d4beecef02119b127ee088ed8fa5678e72080
Dane Späth <dane.spaeth@gmail.com>
2021-11-30 10:35:36 +0100

    Add new script to execute the simulation

---
+++ pypulse/exec_simulation.py
@@ -0,0 +1,25 @@
+from simulation_controller import SimulationController
+from parse_ini import parse_global_ini, parse_ticket
+from datasaver import DataSaver
+from animation_pulsation_new import create_high_res_arrays
+
+
+def exec_sim(ticket):
+    """ Only execute a simulation."""
+    global_dict = parse_global_ini()
+    conf_dict = parse_ticket(ticket)
+    name = str(conf_dict["name"])
+
+    saver = DataSaver(name)
+
+    # Run the Simulation
+    SimulationController(ticket)
+    folder_ticket = saver.copy_ticket_spectrafolder(ticket)
+
+    # Now also save the high res arrays
+    create_high_res_arrays(folder_ticket)
+
+
+if __name__ == "__main__":
+    ticket = "talk_ticket.ini"
+    exec_sim(ticket)
