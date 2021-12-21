3ad24bcba1f2e0840e109228185d4fbb14447ecf
Dane Späth <dane.spaeth@gmail.com>
2021-12-21 12:32:51 +0100

    Improve photometry and add V band

--- pypulse/check_time_series.py
+++ pypulse/check_time_series.py
@@ -16,23 +16,8 @@ def check_time_series(name, instrument=None):
 
             TODO: Change it to use the usual plot functions.
     """
-    # Read in the flux
-    global_dict = parse_global_ini()
-    rvlibpath = global_dict["rvlibpath"]
-    fluxfile = rvlibpath / name / "flux.txt"
-    bjd = []
-    flux = []
-    try:
-        with open(fluxfile, "r") as f:
-            for line in f:
-                columns = line.strip().split()
-                bjd.append(float(columns[0]))
-                flux.append(float(columns[1]))
-    except FileNotFoundError:
-        pass
-    bjd = np.array(bjd)
-    flux = np.array(flux)
-    flux = flux / np.median(flux)
+    # Calculate V band photometry
+    bjd, band_photometry = calc_photometry(name)
 
     # Read in RV, CRX and DLW
     fig, ax = plt.subplots(4, 2, figsize=(20, 10))
@@ -41,9 +26,20 @@ def check_time_series(name, instrument=None):
     dlw_dict = load.dlw(name)
     halpha_dict = load.halpha(name)
 
-    ax[0, 1].plot(bjd, flux / np.median(flux), "bo")
+    ax[0, 1].plot(bjd, band_photometry / np.median(band_photometry), "bo")
     ax[0, 1].set_ylabel("Flux Variation")
     ax[0, 1].set_xlabel("BJD")
+
+    def fluxratio2magdiff(x):
+        return 2.5*np.log10(x)
+
+    def magdiff2fluxratio(x):
+        return 10**(0.4*x)
+
+
+
+    secax = ax[0, 1].secondary_yaxis('right', functions=(fluxratio2magdiff, magdiff2fluxratio))
+    secax.set_ylabel('Magnitude Difference')
 
     plot_rv(rv_dict, ax=ax[0, 0], instrument=instrument)
     plot_activity(crx_dict, ax=ax[1, 0], instrument=instrument)
@@ -73,7 +69,45 @@ def plot_temperature(name):
     plt.show()
 
 
+def calc_photometry(name, band="V"):
+    """ Calculate the fluctuations in the V band."""
+    global_dict = parse_global_ini()
+    # Get raw spectra filesglobal_dict = parse_global_ini()
+    rvlibpath = global_dict["rvlibpath"]
+    RAW_folder = rvlibpath / name / "RAW"
+
+    wave_files = sorted(list(RAW_folder.glob("wave_*.npy")))
+    spec_files = sorted(list(RAW_folder.glob("spec_*.npy")))
+
+    band_photometry = []
+    bjd = []
+
+    for wv_file, sp_file in zip(wave_files, spec_files):
+        wave = np.load(wv_file)
+        spec = np.load(sp_file)
+
+        bjd.append(float(str(wv_file).split("wave_")[-1].split("_")[0]))
+
+        if band == "V":
+            band_center = 5510
+            band_fwhm = 880
+        wave_mask_low = wave > band_center - (band_fwhm / 2)
+        wave_mask_high = wave < band_center + (band_fwhm / 2)
+        wave_mask = np.logical_and(wave_mask_low, wave_mask_high)
+        band_spec = spec[wave_mask]
+
+        band_photometry.append(np.sum(band_spec))
+
+    band_photometry = np.array(band_photometry)
+    return bjd, band_photometry
+
+
 if __name__ == "__main__":
+    # bjd, band_photometry = calc_photometry("TALK_0")
+    # plt.plot(bjd, band_photometry / np.median(band_photometry))
+    # plt.show()
+
     check_time_series("TALK_0")
 
-    # check_time_series("talk_ngc2423_0_dt50_k100_vrot3000_oldtemplate_snronlyheader", instrument="CARMENES_VIS")
+    # check_time_series(
+    #     "talk_ngc2423_0_dt50_k100_vrot3000_oldtemplate_snronlyheader", instrument="CARMENES_VIS")
