6feff1727e912996266608f7bb699ca2d4280e78
Dane Späth <dane.spaeth@gmail.com>
2022-06-03 14:52:03 +0200

    Add first test for radial velocity of granulation

--- pypulse/plot_mean_granulation.py
+++ pypulse/plot_mean_granulation.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from dataloader import granulation_map
 from parse_ini import parse_global_ini
+from physics import spectral_radiance_to_temperature
 
 
 def plot_mean_granulation():
@@ -20,6 +21,45 @@ def plot_mean_granulation():
     fig.colorbar(img, ax=ax, label="Intensity [erg/cm^2/s/A/srad]")
     plt.savefig(tmppath / "random_intensity.png")
     plt.close()
+    
+def test_velocity_map():
+    granulation_spectral_radiance = granulation_map()
+    temperature = spectral_radiance_to_temperature(granulation_spectral_radiance)
+    temp = temperature[1000]
+    v_gran_rad = np.zeros_like(temp)
+    # Crude way to find the granular lanes and the granules
+    dividing_temp = 5100
+    granule_mask = temp >= dividing_temp
+    granular_lane_mask = temp < dividing_temp
+    v_lane = 4500
+    v_granule = -1500
+    v_gran_rad[granular_lane_mask] = v_lane * (dividing_temp - temp[granular_lane_mask]) / (dividing_temp - np.min(temp))
+    v_gran_rad[granule_mask] = v_granule * (dividing_temp - temp[granule_mask]) / (dividing_temp - np.max(temp))
+    i = 0
+    while np.abs(np.mean(v_gran_rad)) > 0.001:
+        i += 1
+        print(i, np.abs(np.mean(v_gran_rad)), v_lane, v_granule)
+        if i > 1e6:
+            break
+        dv = 0.1
+        if np.mean(v_gran_rad) > 0:
+            v_lane -= dv
+            v_granule -= dv
+        else:
+            v_lane += dv
+            v_granule += dv
+        v_gran_rad[granular_lane_mask] = v_lane * (dividing_temp - temp[granular_lane_mask]) / (
+                dividing_temp - np.min(temp))
+        v_gran_rad[granule_mask] = v_granule * (dividing_temp - temp[granule_mask]) / (
+                dividing_temp - np.max(temp))
+
+
+
+    print(np.mean(v_gran_rad))
+    fig, ax = plt.subplots()
+    img = ax.imshow(v_gran_rad, cmap="jet")
+    fig.colorbar(img, ax=ax, label="Velocity [m/s]")
+    plt.show()
 
 if __name__ == "__main__":
-    plot_mean_granulation()
+    test_velocity_map()
