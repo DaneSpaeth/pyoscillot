Fix sign of granulation vector

pypulse/physics.py
@@ -230,11 +230,12 @@ def calc_granulation_velocity_rad(granulation_temp_local):
                                               (DIVIDING_TEMP - np.max(granulation_temp_local))
     return granulation_rad_local
 
-def calc_granulation_velocity_phi_theta(granulation_temp_local):
+def calc_granulation_velocity_phi_theta(granulation_temp_local, vel_rad=None):
     """ Calculate the phi and theta components of the granulation."""
     # Define the areas which are granules
     granule_mask = granulation_temp_local >= DIVIDING_TEMP
-    vel_rad = calc_granulation_velocity_rad(granulation_temp_local)
+    if vel_rad is None:
+        vel_rad = calc_granulation_velocity_rad(granulation_temp_local)
 
     # Determine the size of a cell (will be useful)
     size = granule_mask.shape[0]
@@ -261,17 +262,20 @@ def calc_granulation_velocity_phi_theta(granulation_temp_local):
                 # For the moment assume now horizontal velocity within the inter granular lanes
                 vec = np.array([0, 0])
             else:
-                coord = np.array((row, col))
+                # if row != size or col != size:
+                #     vec = np.array([0, 0])
+                # else:
+                current_pos = np.array((row, col))
                 # Calculate the distance of the complete map from the current pixel
                 # HERE we could still optimize, since that can clearly be computed more efficiently
-                dist = distance_from_px(granule_mask2x2, coord[0], coord[1])
+                dist = distance_from_px(granule_mask2x2, current_pos[0], current_pos[1])
                 # Set the distance of all stuff in a granule to infinite
                 dist[granule_mask2x2] = np.inf
                 # Get the closest non infinite distance
                 min_dist_coords = np.array(np.unravel_index(dist.argmin(), dist.shape))
                 # Compute in vector form and normalize using the radial velocity
-                vec = min_dist_coords - coord
-                normalization = vel_rad2x2[row, col] / np.linalg.norm(vec)
+                vec = min_dist_coords - current_pos
+                normalization = np.abs(vel_rad2x2[row, col]) / np.linalg.norm(vec)
                 vec = vec * normalization
             vec_field[row, col] = vec
 

pypulse/plot_mean_granulation.py
@@ -12,8 +12,6 @@ def plot_mean_granulation():
     tmppath = parse_global_ini()["datapath"] / "tmp_plots"
     granulation = granulation_map()
 
-    print(granulation.shape)
-    exit()
     mean_granulation = np.mean(granulation, axis=0)
     random_intensity = granulation[1592]
     vmin = random_intensity.min()
@@ -43,9 +41,8 @@ def plot_mean_granulation():
 def test_velocity_map():
     granulation_radiance = granulation_map()
     temperature = radiance_to_temperature(granulation_radiance)
-    temp = temperature[1000]
+    temp = temperature[992 + 1292]
     # Crude way to find the granular lanes and the granules
-    dividing_temp = 5100
     vel_phi, vel_theta, vec_field, granule_mask2x2 = calc_granulation_velocity_phi_theta(temp)
 
     size = temp.shape[0]
@@ -57,11 +54,24 @@ def test_velocity_map():
     # imshow "thinks" in row and col (that is also the way the numpy array is defined)
     # but quiver wants x and y, that is essentially the opposite
     # i.e. row corresponds to y and col to x
-    qu = ax.quiver(cols, rows, -vel_phi, vel_theta, np.linalg.norm(vec_field, axis=2), scale=1e5)
+
+    # cols = 50
+    # rows = 50
+    # vel_phi = 100
+    # vel_theta = 0
+    # It makes sense to plot the image with the theta axis going up down, and phi going left, right
+    # Since quive thinks in x and y that mean that vel_theta should be plotted with a minus
+    # but vel_phi not
+    qu = ax.quiver(cols, rows, vel_phi, -vel_theta, np.linalg.norm(vec_field, axis=2), scale=1e5)
+    ax.set_ylabel("Theta (polar angle) (arb. units)")
+    ax.set_xlabel("Phi (azimuthal angle) (arb. units)")
     fig.colorbar(qu, ax=ax, label="Horizontal Velocity [m/s]")
     out_dir = Path("/home/dspaeth/data/simulations/tmp_plots/")
     plt.savefig(out_dir / "vec_field_phi_theta.png", dpi=300)
-    plt.close()
+    # plt.close()
+    plt.show()
+
+    exit()
 
     fig, ax = plt.subplots(1, 2, figsize=(20, 10))
     img = ax[0].imshow(vel_theta, cmap="jet", vmin=-1500, vmax=1500)
@@ -100,4 +110,4 @@ def plot_overlaps():
 
 
 if __name__ == "__main__":
-    plot_mean_granulation()
+    test_velocity_map()