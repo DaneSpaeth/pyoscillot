e0620ab850e15ab4fb02c2bda9d1c0591691cbd8
Dane Späth <dane.spaeth@gmail.com>
2022-08-16 11:02:37 +0200

    Add back in that no telluric mask is used

--- pypulse/run_serval.sh
+++ pypulse/run_serval.sh
@@ -24,6 +24,6 @@ else
     SERVALINST=$INST
 fi
 
-$SERVAL/src/serval.py ${SIMNAME} $DATAPATH/fake_spectra/$SIMNAME/$INST -inst $SERVALINST -targrv 0 -pspline -targ $STAR -brvref DRS -safemode 2
+$SERVAL/src/serval.py ${SIMNAME} $DATAPATH/fake_spectra/$SIMNAME/$INST -inst $SERVALINST -targrv 0 -atmmask "" -pspline -targ $STAR -brvref DRS -safemode 2
 mv $SIMNAME/* $OUTPATH
 rmdir $SIMNAME
