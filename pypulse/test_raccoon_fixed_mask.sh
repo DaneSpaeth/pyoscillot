37caa5c7fa7f220cb05b2a35a101edb42f19f6aa
Dane Späth <dane.spaeth@gmail.com>
2023-05-15 15:36:58 +0200

    Add reduction script with fixed mask for testing

---
+++ pypulse/test_raccoon_fixed_mask.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# activate the venv for racconf
+source ${PYENV_ROOT}/versions/raccoon-venv/bin/activate
+
+RACCOONDATADIR="/home/dspaeth/raccoon/raccoon/data"
+RVLIBPATH="/home/dspaeth/pypulse/data/reduced"
+
+i=84
+fits_dir="/home/dspaeth/pypulse/data/fake_spectra/NGC4349_Test${i}/HARPS/fits"
+BLAZETXTFILE=$fits_dir/"blazefiles.txt"
+
+raccoonccf \
+      $fits_dir/*.fits \
+      HARPS \
+      $RACCOONDATADIR/mask/HARPS/ngc4349-127.mas \
+      --filtell $RACCOONDATADIR/tellurics/CARM_VIS/telluric_mask_carm_short.dat \
+      --filobs2blaze $BLAZETXTFILE \
+      --rvshift none \
+      --fcorrorders obshighsnr \
+      --dirout $RVLIBPATH/raccoon/SIMULATION/NGC4349_Test${i}_ngcmask/HARPS_pre2015_CCF \
+      --dirserval $RVLIBPATH/serval/SIMULATION/NGC4349_Test${i}/HARPS_pre2015 \
+      --plot_sv \
+      --bervmax 100 \
+      --verbose
