875064db7183b84cfc75ef7ae6b135249d455fa5
DaneSpaeth <dane.spaeth@lsw.uni-heidelberg.de>
2023-06-29 14:48:22 +0200

    Add first test of limb dark

--- pypulse/main.py
+++ pypulse/main.py
@@ -169,8 +169,8 @@ if __name__ == "__main__":
 
     # rot_dir = Path(root / "NGC4349_TestRot")
     # tickets = [rot_dir / "test67.ini"]
-    for i in range(109, 111):
-        ticket = root / "NGC4349_TestConvBlue" / f"test{i}.ini"
+    for i in range(111, 112):
+        ticket = root / "NGC4349_TestLimbDark" / f"test{i}.ini"
         tickets.append(ticket)
 
 
