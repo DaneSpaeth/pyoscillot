7583ebff5f20032946832b89476fdf33970f81fa
DaneSpaeth <dane.spaeth@lsw.uni-heidelberg.de>
2023-07-06 14:58:34 +0200

    Add small dT and v_p grid

--- pypulse/main.py
+++ pypulse/main.py
@@ -169,8 +169,8 @@ if __name__ == "__main__":
 
     # rot_dir = Path(root / "NGC4349_TestRot")
     # tickets = [rot_dir / "test67.ini"]
-    for i in [126, 129]:
-        ticket = root / "NGC4349_TestConvBlue" / f"test{i}.ini"
+    for i in range(130, 141):
+        ticket = root / "NGC4349_TestResolution" / f"test{i}.ini"
         tickets.append(ticket)
 
 
