7678d9ab66b20e8db290543a2ff6bfce911b42db
DaneSpaeth <dane.spaeth@lsw.uni-heidelberg.de>
2023-07-13 10:49:04 +0200

    First test of Cubic Spline interpolation

--- pypulse/main.py
+++ pypulse/main.py
@@ -169,8 +169,8 @@ if __name__ == "__main__":
 
     # rot_dir = Path(root / "NGC4349_TestRot")
     # tickets = [rot_dir / "test67.ini"]
-    for i in range(150, 151):
-        ticket = root / "NGC4349_TestMacro" / f"test{i}.ini"
+    for i in range(152, 153):
+        ticket = root / "NGC4349_TestCubicSpline" / f"test{i}.ini"
         tickets.append(ticket)
 
 
