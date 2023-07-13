e50cdef2caae8bc27d77aa41264aea3832bfa3f4
DaneSpaeth <dane.spaeth@lsw.uni-heidelberg.de>
2023-07-13 12:25:56 +0200

    Test ticket

--- pypulse/main.py
+++ pypulse/main.py
@@ -169,7 +169,7 @@ if __name__ == "__main__":
 
     # rot_dir = Path(root / "NGC4349_TestRot")
     # tickets = [rot_dir / "test67.ini"]
-    for i in range(152, 153):
+    for i in range(153, 154):
         ticket = root / "NGC4349_TestCubicSpline" / f"test{i}.ini"
         tickets.append(ticket)
 
