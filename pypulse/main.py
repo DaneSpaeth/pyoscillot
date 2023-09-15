f58c07768be9ebdf7bfda145f0956bd14578e413
DaneSpaeth <dane.spaeth@lsw.uni-heidelberg.de>
2023-09-15 14:13:08 +0200

    Smaller dT

--- pypulse/main.py
+++ pypulse/main.py
@@ -169,7 +169,7 @@ if __name__ == "__main__":
 
     # rot_dir = Path(root / "NGC4349_TestRot")
     # tickets = [rot_dir / "test67.ini"]
-    for i in range(201, 202):
+    for i in range(202, 203):
         ticket = root / "NGC4349_TestMacro" / f"test{i}.ini"
         tickets.append(ticket)
     
