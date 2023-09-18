9f383d02e7a077190d2f714ed64d405f68ec4ed5
DaneSpaeth <dane.spaeth@lsw.uni-heidelberg.de>
2023-09-18 11:35:24 +0200

    Test larger v_rot at small T

--- pypulse/main.py
+++ pypulse/main.py
@@ -169,8 +169,8 @@ if __name__ == "__main__":
 
     # rot_dir = Path(root / "NGC4349_TestRot")
     # tickets = [rot_dir / "test67.ini"]
-    for i in range(206, 207):
-        ticket = root / "NGC4349_TestSampling" / f"test{i}.ini"
+    for i in range(207, 208):
+        ticket = root / "NGC4349_TestMacro" / f"test{i}.ini"
         tickets.append(ticket)
     
     # tickets = [root / "test_V_flux.ini"]
