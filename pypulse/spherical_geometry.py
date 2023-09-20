b87b2e2f8ff679f48448573f88587976e6dacc81
DaneSpaeth <dane.spaeth@lsw.uni-heidelberg.de>
2023-09-20 18:00:45 +0200

    Test extrapolation, add switch (nearest, b-spline)

--- pypulse/spherical_geometry.py
+++ pypulse/spherical_geometry.py
@@ -110,7 +110,8 @@ def project_line_of_sight(phi, theta, values, component, inclination):
 # @jit(nopython=True)
 def project_2d(x, y, z, phi, theta, values, N,
                border=10, component=None, inclination=90,
-               azimuth=0, line_of_sight=False, return_grid_points=False):
+               azimuth=0, line_of_sight=False, return_grid_points=False,
+               edge_extapolation="nearest"):
     """ Project the 3d geometry onto a 2d plane.
 
     https://math.stackexchange.com/questions/2305792/3d-projection-on-a-2d-plane-weak-maths-ressources/2306853
@@ -139,15 +140,15 @@ def project_2d(x, y, z, phi, theta, values, N,
             y_rot[row, col] = vec_rot[1]
             z_rot[row, col] = vec_rot[2]
     
-    # Why do we need that?
+    # Hide the layers on the backside of the star
     x_rot[y_rot < 0] = np.nan
     z_rot[y_rot < 0] = np.nan
     
     # fig = plt.figure(figsize=(9,9))
     # ax = fig.add_subplot(111)
-    # ax.scatter(x_rot, z_rot, c=values, vmin=0, vmax=np.pi)
+    # ax.scatter(x_rot, z_rot, c=values, vmin=4400, vmax=4600)
     # ax.set_xlabel("X'")
-    # ax.set_ylabel("Y'")
+    # ax.set_ylabel("Z'")
     # ax.set_aspect('equal', 'box')
     # plt.savefig("rotation.png", dpi=300)
     # plt.close()
@@ -156,6 +157,7 @@ def project_2d(x, y, z, phi, theta, values, N,
     x_proj = (x_rot - origin[0]) * d / (y_rot - origin[1])
     z_proj = (z_rot - origin[2]) * d / (y_rot - origin[1])
     
+    
     # Since you moved so far back for the projection the values are now very small
     # So we divide by the maximum in x and z (since that is the limb of the star)
     # and should be normalized to one
@@ -163,6 +165,14 @@ def project_2d(x, y, z, phi, theta, values, N,
     x_proj = x_proj / np.nanmax(x_proj)
     z_proj = z_proj / np.nanmax(z_proj)
     
+    # fig = plt.figure(figsize=(9,9))
+    # ax = fig.add_subplot(111)
+    # ax.scatter(x_proj, z_proj, c=values, vmin=4400, vmax=4600)
+    # ax.set_xlabel("X'")
+    # ax.set_ylabel("Z'")
+    # ax.set_aspect('equal', 'box')
+    # plt.savefig("projection.png", dpi=300)
+    # plt.close()
 
     dN = 2 / N
     x_grid = np.linspace(-1 - border * dN, 1 + border * dN, N + 2 * border)
@@ -203,13 +213,30 @@ def project_2d(x, y, z, phi, theta, values, N,
     method = "linear"
     grid = griddata(coords, values, (xx, zz),
                     method=method, fill_value=np.nan)
+    nanmask_grid = np.isnan(grid)
+    
+    # Create the edge_mask
+    dx = xx[0,1] - xx[0,0]
+    distances_from_origin = np.sqrt(xx**2 + zz**2)
+    inside_mask = distances_from_origin <= 1 - np.sqrt(2*(dx/2)**2)
+    outside_mask = distances_from_origin > 1 + np.sqrt(2*(dx/2)**2)
+    edge_mask = np.logical_and(~inside_mask, ~outside_mask)
+    # Also take out the values which are not Nan from the previous interpolation
+    edge_mask = np.logical_and(edge_mask, nanmask_grid)
     
     # Test to also take the cells, whose center is outside the circle
     # Using nearest neighbor interpolation -> not ideal
-    nanmask_grid = np.isnan(grid)
-    grid_nearest = griddata(coords, values, (xx, zz),
-                            method="nearest", fill_value=np.nan)
-    grid[nanmask_grid] = grid_nearest[nanmask_grid]
+    if edge_extapolation == "nearest":
+        grid_nearest = griddata(coords, values, (xx, zz),
+                                method="nearest", fill_value=np.nan)
+        grid[edge_mask] = grid_nearest[edge_mask]
+    elif edge_extapolation == "bispline":
+        tck = bisplrep(x_proj, z_proj, values, kx=3, ky=3)
+        values_bispl = bisplev(x_grid, z_grid, tck).T 
+        grid[edge_mask] = values_bispl[edge_mask]
+    else:
+        raise NotImplementedError
+    
     
     if return_grid_points:
         return grid, xx, zz, nanmask_grid
@@ -256,12 +283,14 @@ if __name__ == "__main__":
     from matplotlib.patches import Circle
     
     
-    def plot_3d_and_proj(phi, theta, xx, yy, zz, savename):
+    def plot_test_weighting(phi, theta, xx, yy, zz, savename):
         N = 50
         x = xx.flatten()
         y = yy.flatten()
         z = zz.flatten()
-        grid, xx, zz, nanmask_grid = project_2d(xx, yy, zz, phi, theta, np.ones_like(phi)*4500, return_grid_points=True, N=N, inclination=90)
+        grid, xx, zz, nanmask_grid = project_2d(xx, yy, zz, phi, theta,
+                                                np.ones_like(phi)*4500, return_grid_points=True,
+                                                N=N, inclination=90, azimuth=0)
         
         fig = plt.figure(figsize=(18,9))
         ax = fig.add_subplot(121, projection='3d')
@@ -274,8 +303,7 @@ if __name__ == "__main__":
         
         # Project
         # ax2.imshow(grid)
-        print(xx.shape)
-        print(zz.shape)
+        
         percentages = percentage_within_circle(xx, zz)
         img = ax2.scatter(xx, zz, marker=".", c=percentages, vmin=0, vmax=1, s=15,)
         xlim = (-1.1, 1.1)
@@ -307,10 +335,70 @@ if __name__ == "__main__":
         
         
         plt.close()
+        
+    def plot_test_extrapolation(phi, theta, xx, yy, zz, savename, edge_extapolation):
+        N = 50
+        x = xx.flatten()
+        y = yy.flatten()
+        z = zz.flatten()
+        grid, xx, zz, nanmask_grid = project_2d(xx, yy, zz, phi, theta, 4500+100*np.sin(phi), return_grid_points=True, N=N, inclination=90, azimuth=0, edge_extapolation=edge_extapolation)
+        
+        fig = plt.figure(figsize=(18,18))
+        ax = fig.add_subplot(221, projection='3d')
+        ax2 = fig.add_subplot(222)
+        ax.scatter(x, y, z, marker=".", c=4500+100*np.sin(phi), vmin=4400, vmax=4600)
+        ax.set_aspect('equal', 'box')
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+        ax.set_zlabel("Z")
+        
+        
+        percentages = percentage_within_circle(xx, zz)
+        grid[percentages <= 0] = np.nan
+        img = ax2.scatter(xx, zz, marker=".", c=grid, vmin=4400, vmax=4600, s=30,)
+        xlim = (-1.1, 1.1)
+        ylim = (-1.1, 1.1)
+        ax2.plot(xx[percentages > 0], zz[percentages > 0], 
+                 c="tab:red", marker="s", fillstyle='none',markersize=8, linestyle="None",)
+        
+        # # ax2.scatter(xx[nanmask_grid], zz[nanmask_grid], marker=".", c=percentages[nanmask_grid], vmin=0, vmax=1, s=15,)
+        # ax2.plot(xx[percentages < 0], zz[percentages < 0], 
+        #          c="gray", marker="s", fillstyle='none',markersize=8, linestyle="None",)
+        
+        ax2.set_aspect('equal', 'box')
+        ax2.set_xlabel("X'")
+        ax2.set_ylabel("Z'")
+        
+        circle = Circle((0, 0), 1, fill=False)
+        ax2.add_patch(circle)
+        
+        ax2.hlines(0, xlim[0], xlim[1], linestyle="--", linewidth=1, color="black")
+        ax2.vlines(0, ylim[0], ylim[1], linestyle="--", linewidth=1, color="black")
+        
+        ax2.set_xlim(xlim)
+        ax2.set_ylim(ylim)
+        
+        from matplotlib import cm
+        ax3 = fig.add_subplot(223, projection="3d")
+        ax3.plot_surface(xx, zz, grid, cmap=cm.viridis,  vmin=4400, vmax=4600,)
+        ax3.azim = 45
+        ax3.elev = 45
+        ax3.set_xlabel("X'")
+        ax3.set_ylabel("Z'")
+        ax3.set_zlabel("T [K]")
+        
+        plt.colorbar(img, label="T [K]")
+        fig.set_tight_layout(True)
+        plt.savefig(savename, dpi=300)
+        
+        
+        plt.close()
     
     # Test a sphere
     phi, theta, xx, yy, zz = get_spherical_phi_theta_x_y_z(N=151)
-    plot_3d_and_proj(phi, theta, xx, yy, zz, "3D_cloud.png")
+    plot_test_extrapolation(phi, theta, xx, yy, zz, "3D_cloud_test_extrapol_nearest.png", "nearest")
+    plot_test_extrapolation(phi, theta, xx, yy, zz, "3D_cloud_test_extrapol_bispline.png", "bispline")
+    
     
     # Test a cube
     # x = np.linspace(-3, 3, 25)
