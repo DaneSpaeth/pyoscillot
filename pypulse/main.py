4699b4c188a069302c898a6fe0b05b2174049ee9
DaneSpaeth <dane.spaeth@lsw.uni-heidelberg.de>
2023-10-04 16:26:44 +0200

    Add test for resolution adjustment

--- pypulse/main.py
+++ pypulse/main.py
@@ -169,14 +169,14 @@ if __name__ == "__main__":
 
     # rot_dir = Path(root / "NGC4349_TestRot")
     # tickets = [rot_dir / "test67.ini"]
-    # for i in range(222, 223):
-    #     ticket = root / "NGC4349_TestMacro" / f"test{i}.ini"
-    #     tickets.append(ticket)
+    for i in range(223, 224):
+        ticket = root / "NGC4349_TestMacro" / f"test{i}.ini"
+        tickets.append(ticket)
     
     # tickets = [root / "test_V_flux.ini"]
     
-    grid_folder = root / "NGC4349_blindsearch_grid"
-    tickets = sorted(list(grid_folder.glob("*_blindsearch_grid_*.ini")))
+    # grid_folder = root / "NGC4349_blindsearch_grid"
+    # tickets = sorted(list(grid_folder.glob("*_blindsearch_grid_*.ini")))
 
     for ticket in tickets:
         try:
