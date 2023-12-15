1977fd7c3af6618fdde50f7ebc6ff36b5716d6d3
DaneSpaeth <dane.spaeth@lsw.uni-heidelberg.de>
2023-12-15 10:21:06 +0100

    Fix grid to be at i=90°

--- pypulse/grid_simulator.py
+++ pypulse/grid_simulator.py
@@ -8,8 +8,13 @@ global_dict = parse_global_ini()
 TICKETROOT = global_dict["ticketpath"]
 
 #### INSERT YOUR NAMES ETC HERE ####
-gridname = "NGC4349_l2_m-2_grid"
+gridname = "NGC4349_l2_m-2_20K_grid"
 baseticket = TICKETROOT / gridname / "base.ini"
+
+v_ps = [0.29847640045 + i*0.05 for i in range(-2, 5)]
+
+# print(v_ps)
+# exit()
 
 #### DEFINE THE RANGES THAT YOU WANT TO SIMULATE ####
 # dt = np.arange(21, 24, 1, dtype=int)
@@ -39,9 +44,10 @@ baseticket = TICKETROOT / gridname / "base.ini"
 # CB_models = ['alpha_boo', 'alpha_ari', 'alpha_sct', 'alpha_ser', 'alpha_uma', 'beta_boo', 'beta_cet', 'beta_gem', 'beta_oph', 'delta_dra', 'epsilon_cyg', 'epsilon_hya', 'epsilon_vir', 'eta_cyg', 'eta_dra', 'eta_her', 'eta_ser', 'gamma_psc', 'gamma_tau', 'iota_cep', 'kappa_cyg', 'kappa_per', 'mu_peg', 'nu_oph', 'nu_uma', 'rho_boo', 'xi_her', 'zeta_cyg']
 
 
-dTs = [0., 10.0, 20.0, 30.0, 40.0, 49.9]
-idx_plus = 6
-for idx, dT in enumerate(dTs):
+
+idx_plus = 0
+
+for idx, v_p in enumerate(v_ps):
     
     config = configparser.ConfigParser()
     config.read(baseticket)
@@ -51,10 +57,10 @@ for idx, dT in enumerate(dTs):
     config["GLOBAL"]["name"] = simname
     config["GLOBAL"]["date"] = datetime.today().strftime("%d.%m.%Y")
     # config["GLOBAL"]["v_rot"] = str(v_rot)
-    # config["pulsation"]["v_p"] = str(v_p)
+    config["pulsation"]["v_p"] = str(v_p)
     # config["GLOBAL"]["inclination"] = str(inclination)
-    config["pulsation"]["dt"] = str(round(dT, 1))
-    config["pulsation"]["t_phase"] = str(180.)
+    # config["pulsation"]["dt"] = str(round(dT, 1))
+    # config["pulsation"]["t_phase"] = str(180.)
     # config["pulsation"]["l"] = str(l)
     # config["pulsation"]["m"] = str(m)
     
