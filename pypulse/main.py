a7f48645d707c2c331125022c698d957d38dbb94
DaneSpaeth <dane.spaeth@lsw.uni-heidelberg.de>
2023-12-19 12:27:22 +0100

    Add combined l=1 modes

--- pypulse/main.py
+++ pypulse/main.py
@@ -162,16 +162,18 @@ if __name__ == "__main__":
     # ticket = root / "NGC4349_fine_tuning" / "NGC4349_improved_fine_grid_163+3.ini"
     # main(ticket, run=True, serval=True, raccoon=True, run_laptop=False)
     # exit()
-    # root = Path().cwd() / "tickets"
-    # gridname = "NGC4349_l2_m-2_20K_grid"
+    root = Path().cwd() / "tickets"
+    gridname = "NGC4349_l2_m-2_20K_grid"
     # grid_folder = root / gridname
     # tickets = sorted(list(grid_folder.glob(f"{gridname}*.ini")))
     
-    tickets = [Path("/home/dspaeth/pypulse/pypulse/tickets/NGC4349_very_fine_RV_grid/NGC4349_very_fine_RV_grid_022+02+01.ini")]
+    # tickets = [Path("/home/dspaeth/pypulse/pypulse/tickets/NGC4349_very_fine_RV_grid/NGC4349_very_fine_RV_grid_022+02+01.ini")]
     
     # second_grid_folder = root / "NGC4349_l1m0_broad_grid"
     # second_tickets = sorted(list(second_grid_folder.glob("NGC4349_*.ini")))
     # tickets = tickets + second_tickets
+    gridname = "combined_modes"
+    tickets = sorted(list((root / gridname).glob("*.ini")))
     
     for idx, ticket in enumerate(tickets):
         # if idx <= 13:
