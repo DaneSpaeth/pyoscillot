eef07b322bc6bd89b9ebf9f68913e09f80872a75
DaneSpaeth <dane.spaeth@lsw.uni-heidelberg.de>
2024-01-24 11:12:55 +0100

    Remove get_circular_mask function

--- pyoscillot/utils.py
+++ pyoscillot/utils.py
@@ -16,28 +16,6 @@ import copy
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-# mpl.use('TkAgg')
-
-
-
-def create_circular_mask(h, w, center=None, radius=None):
-    """ Create a circular mask.
-
-        From https://stackoverflow.com/questions/44865023/how-can-i-create-a-circular-mask-for-a-numpy-array
-    """
-
-    if center is None:
-        # use the middle of the image
-        center = (int(w / 2), int(h / 2))
-    if radius is None:
-        # use the smallest distance between the center and image walls
-        radius = min(center[0], center[1], w - center[0], h - center[1])
-
-    Y, X = np.ogrid[:h, :w]
-    dist_from_center = np.sqrt((X - center[0])**2 + (Y - center[1])**2)
-
-    mask = dist_from_center <= radius
-    return mask
 
 
 def gaussian(x, mu=0, sigma=0.001):
