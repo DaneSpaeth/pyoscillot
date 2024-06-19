77ed3d5404eaea77dd8897bdaaa7a5217a26585a
DaneSpaeth <dane.spaeth@lsw.uni-heidelberg.de>
2024-06-19 16:04:05 +0200

    Update plot settings

--- pyoscillot/plot_settings.py
+++ pyoscillot/plot_settings.py
@@ -16,10 +16,12 @@ BIGGER_SIZE = 12
 FULL_PAGE_WIDTH = 7.08661
 # 88 mm in inch from A&A
 HALF_PAGE_WIDTH = 3.46457
+# Measured for PhD thesis
+THESIS_WIDTH = 5.8476
 
-plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
-plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
-plt.rc('axes', labelsize=SMALL_SIZE)    # fontsize of the x and y labels
+plt.rc('font', size=MEDIUM_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=MEDIUM_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
 plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
